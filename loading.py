a3993f91bd85cb78441da2fa6dee97d524a1e1d2
someshfengde <someshfengde@gmail.com>
2021-06-08 16:15:22 +0530

    solved some bugs

--- loading.py
+++ loading.py
@@ -32,14 +32,14 @@ def preprocess_buffer(buffer):
     image = Image.open(buffer)
     image_array = np.array(image)
     resized_image = tf.image.resize(image_array, [224, 224])
-    return resized_image
+    return resized_image,image
 
 
 def predict_and_view(buffer, model_path):
     model = load_model(model_path)
-    resized_image = preprocess_buffer(buffer)
+    resized_image,buffered_image = preprocess_buffer(buffer)
     expanded_image = tf.expand_dims(resized_image, axis=0)
     prediction = model.predict(expanded_image)
     prediction_name = class_names[prediction.argmax(axis=1)[0]]
     prediction_accuracy = prediction[0][prediction.argmax(axis=1)[0]]
-    return expanded_image, prediction_name, prediction_accuracy
+    return buffered_image, prediction_name, prediction_accuracy
